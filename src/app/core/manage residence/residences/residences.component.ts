a29d3f00abfbcc5f3a6ceadb47bb7b30d529682d
radhouene-massoudi <massoudiradhouene@gmail.com>
2023-11-09 16:12:38 +0100

    seance 2

--- src/app/core/manage residence/residences/residences.component.ts
+++ src/app/core/manage residence/residences/residences.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Apartment } from 'src/app/model/Apartment';
 import { Residences } from 'src/app/model/residence';
 
 @Component({
@@ -17,4 +18,9 @@ export class ResidencesComponent {
     {id:4,"name": "El Anber","address":"Manzah 5",
     "image":"../../../assets/images/R4.jpg"}
     ];
+
+    listApartments:Apartment[]=[
+      {id:1,"appartNum":1,"floorNum":0,"surface":100,"terrace":"oui","surfaceTerrace":20,"category":"S+1","description":"AppartementS+1","residence":this.listResidences[0] },
+      {id:2,"appartNum":2,"floorNum":0,"surface":130,"terrace":"non","surfaceTerrace":0,"category":"S+2","description":"Appartement S+2","residence":this.listResidences[0] },
+      {id:3,"appartNum":3,"floorNum":0,"surface":150,"terrace":"oui","surfaceTerrace":30,"category":"S+3","description":"Appartement S+3","residence":this.listResidences[1] }];
 }
