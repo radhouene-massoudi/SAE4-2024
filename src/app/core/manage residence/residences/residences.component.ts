adcc50ae122c7ca48e38905b24a7cdd51746e70b
radhouene-massoudi <massoudiradhouene@gmail.com>
2023-11-09 16:43:11 +0100

    seance 2

--- src/app/core/manage residence/residences/residences.component.ts
+++ src/app/core/manage residence/residences/residences.component.ts
@@ -8,19 +8,37 @@ import { Residences } from 'src/app/model/residence';
   styleUrls: ['./residences.component.css']
 })
 export class ResidencesComponent {
-  listResidences:Residences[]=[
-    {id:1,"name": "El fel","address":"Borj Cedria",
-    "image":"../../../assets/images/R1.jpeg"},
-    {id:2,"name": "El yasmine",
-    "address":"Ezzahra","image":"../../../assets/images/R2.jpg"},
-    {id:3,"name": "El Arij",
-    "address":"Rades","image":"../../../assets/images/R3.jpg"},
-    {id:4,"name": "El Anber","address":"Manzah 5",
-    "image":"../../../assets/images/R4.jpg"}
-    ];
+  app:Apartment[] = [];
+  listResidences: Residences[] = [
+    {
+      id: 1, "name": "El fel", "address": "Borj Cedria",
+      "image": "../../../assets/images/R1.jpeg"
+    },
+    {
+      id: 2, "name": "El yasmine",
+      "address": "Ezzahra", "image": "../../../assets/images/R2.jpg"
+    },
+    {
+      id: 3, "name": "El Arij",
+      "address": "Rades", "image": "../../../assets/images/R3.jpg"
+    },
+    {
+      id: 4, "name": "El Anber", "address": "Manzah 5",
+      "image": "../../../assets/images/R4.jpg"
+    }
+  ];
 
-    listApartments:Apartment[]=[
-      {id:1,"appartNum":1,"floorNum":0,"surface":100,"terrace":"oui","surfaceTerrace":20,"category":"S+1","description":"AppartementS+1","residence":this.listResidences[0] },
-      {id:2,"appartNum":2,"floorNum":0,"surface":130,"terrace":"non","surfaceTerrace":0,"category":"S+2","description":"Appartement S+2","residence":this.listResidences[0] },
-      {id:3,"appartNum":3,"floorNum":0,"surface":150,"terrace":"oui","surfaceTerrace":30,"category":"S+3","description":"Appartement S+3","residence":this.listResidences[1] }];
+  listApartments: Apartment[] = [
+    { id: 1, "appartNum": 1, "floorNum": 0, "surface": 100, "terrace": "oui", "surfaceTerrace": 20, "category": "S+1", "description": "AppartementS+1", "residence": this.listResidences[0] },
+    { id: 2, "appartNum": 2, "floorNum": 0, "surface": 130, "terrace": "non", "surfaceTerrace": 0, "category": "S+2", "description": "Appartement S+2", "residence": this.listResidences[0] },
+    { id: 3, "appartNum": 3, "floorNum": 0, "surface": 150, "terrace": "oui", "surfaceTerrace": 30, "category": "S+3", "description": "Appartement S+3", "residence": this.listResidences[1] }];
+  showApp(j:any) {
+    
+    for (let i = 0; i < this.listApartments.length; i++) {
+     let  index=this.listResidences.indexOf(this.listApartments[i].residence)
+if(j=index){
+  this.app.push(this.listApartments[i]);
+}
+    }
+  }
 }
